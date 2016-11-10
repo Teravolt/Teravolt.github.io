1a2622aa296c202a86be7f1f2affb4673d54e14b
Pavan Kantharaju <pavank92@gmail.com>
2016-11-10 10:53:15 -0500

    Resetting website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("navMenu").innerHTML =
+    '<ul id="navi">'+
+        '<li><a class="active" href="index.html">Home</a></li>' + 
+        '<li><a href="projects.html">Projects</a></li>' +
+        '<li><a href="contact.html">Contact</a></li>' + 
+        '<li><a href="about.html">About</a></li>' +
+    '</ul>';
